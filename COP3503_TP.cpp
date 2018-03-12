dc0b16cd671fbd4ede1ebba8dda800c72c29c5e1
drivera2960 <drivera2960@ufl.edu>
2018-03-12 17:30:46 -0400

    removed unfinished class

--- COP3503_TP.cpp
+++ COP3503_TP.cpp
@@ -16,22 +16,26 @@ string line;
 
 int main(){
 
-cout<< fixed << setprecision(2); //Sets cout to output numbers with 2 decimal places
+cout<< fixed << setprecision(2);	//Sets cout to output numbers with 2 decimal places
 
-Store supermarket = Store();	//Creates object <supermarket>
+Store supermarket = Store();		//Creates object <supermarket>
 
-ifstream productDataFile;
-productDataFile.open("FOOD.txt");
+ifstream productDataFile;			//Initializes productDataFile as a istream object
+productDataFile.open("FOOD.txt");	//Data will be streamed from the <FOOD.txt> file
 
-// Saves one line of the text file into "line", and checks if eof has been reached.
+// Saves one line of the text file into "line", until eof (end of file) is reached.
 while(getline(productDataFile, line)){
 
-	vector<string> lineVec;
-	istringstream iss(line);
 
+	vector<string> lineVec;			//<lineVec> is a vector that will be used to store the split strings
+									//from the string "line"
+	istringstream iss(line);		//create a string stream "iss" to operate on the string line.
+
+//While-loop will split "line" by the spaces it contains.  It will store the splitted strings into "lineVec"
 	while( iss >> line )
 	    lineVec.push_back(line);
 
+//The following bool expressions check what sections the each entry belongs to.
 	if (lineVec.at(1).compare("produce") == 0){
 			supermarket.addProduce(lineVec.at(0), lineVec.at(1), lineVec.at(2), stod(lineVec.at(3)));
 		}
@@ -41,7 +45,7 @@ while(getline(productDataFile, line)){
 
 	lineVec.clear();
 }
-	productDataFile.close();
+	productDataFile.close();	//Closes the stream of productDataFile when the eof has been reached.
 
 	supermarket.listInfo();
 	return 0;
